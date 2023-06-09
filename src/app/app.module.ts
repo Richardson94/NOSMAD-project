f9849e307953c8f90b85be58a0bd6721dd8abc39
Richardson94 <richcori94@gmail.com>
2023-06-09 17:03:07 -0400

    adding nebular module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-//import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './home/home.module';
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    //  AppRoutingModule
-  ],
+  imports: [BrowserModule, HomeModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
