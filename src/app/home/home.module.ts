b248a1bd1f3f5ecea22de85efe6b4c3aeb189081
Richardson94 <richcori94@gmail.com>
2023-06-21 23:22:22 -0400

    fix issue with nebular

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import { NebularModule } from '../nebular/nebular.module';
+//import { NebularModule } from '../nebular/nebular.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, NebularModule],
+  imports: [
+    CommonModule,
+    //NebularModule
+  ],
   exports: [HomeComponent],
 })
 export class HomeModule {}
--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './navbar.component';
-import { NebularModule } from '../nebular/nebular.module';
+//import { NebularModule } from '../nebular/nebular.module';
 
 @NgModule({
   declarations: [NavBarComponent],
-  imports: [CommonModule, NebularModule],
+  imports: [
+    CommonModule,
+    //NebularModule
+  ],
   exports: [NavBarComponent],
 })
 export class NavbarModule {}
